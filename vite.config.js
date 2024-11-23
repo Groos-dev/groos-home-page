7da2437827ee583980e4606ccec584799f744278
groos <gegu926@gmial.com>
2024-11-23 12:45:54 +0800

    feat: 调整vite.config.js 的配置

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 设置相对路径
   plugins: [react()],
+  build: {
+    rollupOptions: {
+    },
+  },
 })
